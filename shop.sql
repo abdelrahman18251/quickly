c80ec8942d19569a4d41267efd1cc7377d881c85
p-mohamed-elsawy <m.elsawy1996@gmail.com>
2019-08-24 07:27:47 +0200

    upload db

--- shop.sql
+++ shop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Aug 20, 2019 at 10:39 PM
+-- Generation Time: Aug 24, 2019 at 07:26 AM
 -- Server version: 10.3.16-MariaDB
 -- PHP Version: 7.3.6
 
@@ -116,6 +116,15 @@ CREATE TABLE `comments` (
   `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`c_id`, `comment`, `status`, `comment_date`, `item_id`, `user_id`) VALUES
+(16, '131563', 0, '2019-08-23', 51, 176),
+(17, '456456456456456', 0, '2019-08-23', 51, 176),
+(18, '456456456456456', 0, '2019-08-23', 51, 176);
+
 -- --------------------------------------------------------
 
 --
@@ -152,18 +161,35 @@ INSERT INTO `engineer` (`UserID`, `Username`, `Password`, `Email`, `FullName`, `
 CREATE TABLE `items` (
   `Item_ID` int(11) NOT NULL,
   `Name` varchar(255) NOT NULL,
-  `Description` text NOT NULL,
-  `Price` varchar(255) NOT NULL,
-  `Add_Date` date NOT NULL,
-  `Country_Made` varchar(255) NOT NULL,
-  `Image` varchar(255) NOT NULL,
-  `Status` varchar(255) NOT NULL,
-  `Rating` smallint(6) NOT NULL,
+  `Description` text DEFAULT NULL,
+  `Price` varchar(255) DEFAULT NULL,
+  `Add_Date` text NOT NULL,
+  `Country_Made` varchar(255) DEFAULT NULL,
+  `Image` varchar(255) DEFAULT NULL,
+  `Status` varchar(255) NOT NULL COMMENT '0 / hide   1/ show',
+  `Rating` smallint(6) DEFAULT NULL,
   `Approve` tinyint(4) NOT NULL DEFAULT 0,
   `Cat_ID` int(11) NOT NULL,
-  `Member_ID` int(11) NOT NULL,
-  `tags` varchar(255) DEFAULT NULL
+  `Member_ID` int(11) DEFAULT NULL,
+  `tags` varchar(255) DEFAULT NULL,
+  `address` longtext NOT NULL,
+  `mobile` varchar(150) NOT NULL,
+  `phone` varchar(150) NOT NULL,
+  `types` varchar(150) NOT NULL,
+  `brands` varchar(150) NOT NULL,
+  `brand` varchar(150) NOT NULL,
+  `defect` longtext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `items`
+--
+
+INSERT INTO `items` (`Item_ID`, `Name`, `Description`, `Price`, `Add_Date`, `Country_Made`, `Image`, `Status`, `Rating`, `Approve`, `Cat_ID`, `Member_ID`, `tags`, `address`, `mobile`, `phone`, `types`, `brands`, `brand`, `defect`) VALUES
+(50, 'Kaseem Pace', 'Vitae expedita dolorVitae expedita dolorVitae expedita dolorVitae expedita dolorVitae expedita dolorVitae expedita dolorVitae expedita dolorVitae expedita dolorVitae expedita dolorVitae expedita dolorVitae expedita dolorVitae expedita dolorVitae expedita dolorVitae expedita dolorVitae expedita dolorVitae expedita dolorVitae expedita dolor', '457457', '2019-08-23 21:36:31', 'aa', 'img', '1', NULL, 1, 23, 175, NULL, 'Nemo rerum quis quis', '67807807806', '+1 (765) 357-5344', 'أجهزة منزلية', 'fgjdfgj', '5', 'Vitae expedita dolorVitae expedita dolorVitae expedita dolorVitae expedita dolorVitae expedita dolorVitae expedita dolorVitae expedita dolorVitae expedita dolorVitae expedita dolorVitae expedita dolorVitae expedita dolorVitae expedita dolorVitae expedita dolorVitae expedita dolorVitae expedita dolorVitae expedita dolorVitae expedita dolor'),
+(51, 'Victor Cunningham', ' Tempore ipsum reruTempore ipsum reruTempore ipsum reruTempore ipsum reruTempore ipsum reruTempore ipsum reruTempore ipsum reruTempore ipsum reru', '457547', '2019-08-23 21:36:58', 'eg', 'img', '1', NULL, 1, 23, 168, NULL, 'Saepe voluptatem ven', '76897695695', '+1 (483) 802-8762', 'تكييف مركزى', 'الأجهزة المصرية', '8', ' Tempore ipsum reruTempore ipsum reruTempore ipsum reruTempore ipsum reruTempore ipsum reruTempore ipsum reruTempore ipsum reruTempore ipsum reru'),
+(52, 'Diana Cantu', 'Anim cum voluptatem Anim cum voluptatem Anim cum voluptatem Anim cum voluptatem Anim cum voluptatem Anim cum voluptatem Anim cum voluptatem Anim cum voluptatem Anim cum voluptatem Anim cum voluptatem Anim cum voluptatem Anim cum voluptatem ', '457457', '2019-08-23 21:37:34', 'rr', 'img', '0', NULL, 1, 23, 169, NULL, 'Mollitia aut enim ip', '65845864568', '+1 (299) 646-9641', 'أجهزة منزلية', 'الأجهزة الايطالية', '4', 'Anim cum voluptatem Anim cum voluptatem Anim cum voluptatem Anim cum voluptatem Anim cum voluptatem Anim cum voluptatem Anim cum voluptatem Anim cum voluptatem Anim cum voluptatem Anim cum voluptatem Anim cum voluptatem Anim cum voluptatem '),
+(53, 'ELSAWY', 'Omnis laborum sint i', NULL, '2019-08-23 21:49:28', NULL, 'img', '1', NULL, 1, 23, NULL, NULL, 'In illo exercitation', '76967967967', '+1 (318) 525-3759', 'أجهزة منزلية', 'الأجهزة الاسبانية', '2', 'Omnis laborum sint i');
 
 -- --------------------------------------------------------
 
@@ -231,7 +257,21 @@ INSERT INTO `users` (`UserID`, `Username`, `Password`, `Email`, `FullName`, `Gro
 (159, 'munyxat', 'Pa$$w0rd!', 'luxok@mailinator.com', 'Gil Shaw', 0, 0, 0, '2019-08-20 20:26:55', NULL, '4574574', 'Aut eu eveniet amet', '', '1', '5', '', '3', '', '', '4574', '547457457', ''),
 (160, 'fezam', 'ac748cb38ff28d1ea98458b16695739d7e90f22d', 'ziqago@mailinator.net', 'Ocean Le', 0, 0, 0, '2019-08-20 20:33:42', NULL, '', 'Possimus quod assum', '', '1', '12', '', '6', '43636463643636', 'نظام 5 سنوات صنايعي', '', '', ''),
 (161, 'kigetepy', 'ac748cb38ff28d1ea98458b16695739d7e90f22d', 'rejanonut@mailinator.com', 'Lee Meadows', 0, 0, 0, '2019-08-20 20:35:34', NULL, '', 'Voluptas incidunt l', '', '1', '12', '', '', '', '', '', '', ''),
-(162, 'pujydukone', 'ac748cb38ff28d1ea98458b16695739d7e90f22d', 'lovyr@mailinator.net', 'Lance Wiggins', 0, 0, 0, '2019-08-20 20:36:22', NULL, '', 'Est do officiis non', '', '1', '13', '', '', '', '', '', '', '');
+(162, 'pujydukone', 'ac748cb38ff28d1ea98458b16695739d7e90f22d', 'lovyr@mailinator.net', 'Lance Wiggins', 0, 0, 0, '2019-08-20 20:36:22', NULL, '', 'Est do officiis non', '1', '1', '13', '', '', '', '', '', '', ''),
+(163, 'jeryhideq', 'ac748cb38ff28d1ea98458b16695739d7e90f22d', 'waryhav@mailinator.com', 'Jorden Joyner', 0, 0, 0, '2019-08-20 21:25:29', NULL, '', 'Facilis vel inventor', '', '2', '4', '', '1', '35235235235235', 'معهد فني صنايعي', '', '', ''),
+(164, 'gobenogufo', 'ac748cb38ff28d1ea98458b16695739d7e90f22d', 'kularyz@mailinator.net', 'Samantha Brennan', 0, 0, 0, '2019-08-20 21:26:27', NULL, '', 'Sed sed voluptatibus', '', '1', '9', '', '', '', '', '', '', ''),
+(165, 'carofot', 'ac748cb38ff28d1ea98458b16695739d7e90f22d', 'zykyzyhime@mailinator.com', 'Brenden Serrano', 0, 0, 0, '2019-08-20 21:27:42', NULL, '', 'Autem do ex neque al', '', '2', '3', '', '', '', '', '', '', ''),
+(166, 'jicura', 'ac748cb38ff28d1ea98458b16695739d7e90f22d', 'pobe@mailinator.net', 'Cooper Boyd', 0, 0, 0, '2019-08-20 21:29:15', NULL, '', 'Unde fugiat quaerat', 'الفيوم', '1', '14', '', '', '', '', '', '', ''),
+(167, 'golanyvut', 'ac748cb38ff28d1ea98458b16695739d7e90f22d', 'qyruzyrowo@mailinator.com', 'Kirsten Rivera', 0, 0, 0, '2019-08-20 21:42:58', NULL, '+1 (282) 271-4324', 'Qui laudantium sint', 'اسوان', 'أجهزة منزلية', 'الأجهزة الاسبانية', '4', 'وكيلاء تجاريون', '', '', '5', 'Aut voluptatem eos ', '47457474575'),
+(168, 'mifeb', 'ac748cb38ff28d1ea98458b16695739d7e90f22d', 'cyre@mailinator.com', 'Evangeline Sellers', 0, 0, 0, '2019-08-20 21:46:52', NULL, '', 'Eius sunt non omnis', 'بني سويف ', 'أجهزة منزلية', 'fgjdfgj', '', '', '', '', '', '', ''),
+(169, 'gagenunu', 'ac748cb38ff28d1ea98458b16695739d7e90f22d', 'fypynu@mailinator.com', 'Knox Pace', 0, 0, 0, '2019-08-20 21:55:50', NULL, '', 'Eu culpa quo dolor i', '14', 'تكييف مركزى', 'الأجهزة الأمريكية', '', '', '', '', '', '', ''),
+(170, 'trtur5474', '4b8aaf27d22e5b15e5da3c911521ab4715494808', 'aafsafs@sdggd.com', 'fjgfdjfdgjdfgjdfgj', 0, 0, 0, '2019-08-20 21:59:06', NULL, '', 'fdhdf dfhdfh', '10', 'أجهزة منزلية', '', '', '', '', '', '', '', ''),
+(171, 'hohumojema', 'ac748cb38ff28d1ea98458b16695739d7e90f22d', 'novof@mailinator.net', 'Jescie Hood', 0, 0, 0, '2019-08-20 22:18:44', NULL, '685868', 'Expedita itaque exer', '10', 'تكييف مركزى', 'الأجهزة المصرية', '', 'مركز خدمة متخصص', '', '', '568568', '56858568', ''),
+(172, 'qezanu', 'ac748cb38ff28d1ea98458b16695739d7e90f22d', 'ditywub@mailinator.com', 'Kieran Mayer', 0, 0, 0, '2019-08-20 22:21:19', NULL, '', 'Mollitia non cumque ', '2', 'تكييف مركزى', 'الأجهزة الأمريكية', '', '', '', '', '', '', ''),
+(173, 'rixymi', 'ac748cb38ff28d1ea98458b16695739d7e90f22d', 'fozafutah@mailinator.net', 'James Harrington', 0, 0, 0, '2019-08-20 22:41:43', NULL, '', 'Est assumenda volupt', '9', 'أجهزة منزلية', 'الأجهزة الايطالية', '8', 'وكيلاء تجاريون', '', '', '', '', ''),
+(174, 'lonufoso', 'ac748cb38ff28d1ea98458b16695739d7e90f22d', 'zosori@mailinator.com', 'Ishmael Kent', 0, 0, 0, '2019-08-20 22:58:18', NULL, '', 'Excepteur molestiae ', '28', 'أجهزة منزلية', 'الأجهزة المصرية', '', '', '', '', '', '', ''),
+(175, 'qipybixu', 'ac748cb38ff28d1ea98458b16695739d7e90f22d', 'sifi@mailinator.net', 'Lucy Douglas', 0, 0, 0, '2019-08-21 01:47:21', NULL, '457474575', 'Cupiditate et quo ev', '9', 'أجهزة منزلية', 'الأجهزة اليابانية', '', 'مركز خدمة معتمد', '', '', '547', '47457457', ''),
+(176, 'rojyh', 'ac748cb38ff28d1ea98458b16695739d7e90f22d', 'maleredu@mailinator.com', 'Ifeoma Glenn', 0, 0, 0, '2019-08-23 16:49:24', NULL, '', 'Exercitationem enim ', '23', 'تكييف مركزى', 'dfhdfh', '4', 'فني كنترول', '34634634634', 'مؤهل عالي', '', '', '');
 
 --
 -- Indexes for dumped tables
@@ -301,13 +341,13 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `items`
 --
 ALTER TABLE `items`
-  MODIFY `Item_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `Item_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
 
 --
 -- AUTO_INCREMENT for table `manufactureCountrys`
@@ -319,7 +359,7 @@ ALTER TABLE `manufactureCountrys`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT COMMENT 'To Identify User', AUTO_INCREMENT=163;
+  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT COMMENT 'To Identify User', AUTO_INCREMENT=177;
 
 --
 -- Constraints for dumped tables
